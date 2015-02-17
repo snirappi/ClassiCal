5076c5b52ce3ed7e9133b5dac15b865a90adfdaf
mtdtao <mtdtao@gmail.com>
2015-02-17 16:04:10 -0500

    ClassiCal iOS project

---
+++ iOS/ClassiCal/ClassiCal/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ClassiCal
+//
+//  Created by 曾 锦涛 on 2/17/15.
+//  Copyright (c) 2015 CS 307 Team 8. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
