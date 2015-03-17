8baaf593ca2e46614b962ee8f8fac8a62e062ab7
Tim Vincent <vincent3@purdue.edu>
2015-03-17 16:16:59 -0400

    Implemented Android Login activity

--- Android/Application/build/generated/source/r/debug/com/team3/classical/slidingtabs/R.java
+++ Android/Application/build/generated/source/r/debug/com/team3/classical/slidingtabs/R.java
@@ -23,36 +23,41 @@ public final class R {
     }
     public static final class drawable {
         public static final int action_search=0x7f020000;
-        public static final int background=0x7f020001;
-        public static final int cal=0x7f020002;
-        public static final int drawer_shadow=0x7f020003;
-        public static final int ic_drawer=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int tile=0x7f020006;
+        public static final int cal=0x7f020001;
+        public static final int drawer_shadow=0x7f020002;
+        public static final int ic_drawer=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int tile=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080015;
-        public static final int buttonSend=0x7f08000a;
-        public static final int chatText=0x7f08000b;
-        public static final int chatTextField=0x7f080009;
-        public static final int item_title=0x7f080013;
-        public static final int last=0x7f080011;
-        public static final int linearLayout1=0x7f080008;
-        public static final int list=0x7f08000c;
-        public static final int ll1=0x7f080002;
-        public static final int ll2=0x7f080003;
-        public static final int ll3=0x7f080004;
-        public static final int ll4=0x7f080005;
-        public static final int ll5=0x7f080006;
-        public static final int menu_toggle_log=0x7f080014;
-        public static final int place=0x7f080010;
-        public static final int sample_content_fragment=0x7f080001;
-        public static final int sample_main_layout=0x7f080000;
-        public static final int sliding_tabs=0x7f08000d;
-        public static final int textOutput2=0x7f080007;
-        public static final int time=0x7f080012;
-        public static final int title=0x7f08000f;
-        public static final int viewpager=0x7f08000e;
+        public static final int action_settings=0x7f05001b;
+        public static final int buttonSend=0x7f05000b;
+        public static final int chatText=0x7f05000c;
+        public static final int chatTextField=0x7f05000a;
+        public static final int editText=0x7f050014;
+        public static final int email=0x7f050016;
+        public static final int imageView=0x7f050015;
+        public static final int item_title=0x7f050019;
+        public static final int last=0x7f050012;
+        public static final int linearLayout1=0x7f050009;
+        public static final int list=0x7f05000d;
+        public static final int ll1=0x7f050003;
+        public static final int ll2=0x7f050004;
+        public static final int ll3=0x7f050005;
+        public static final int ll4=0x7f050006;
+        public static final int ll5=0x7f050007;
+        public static final int login=0x7f050000;
+        public static final int menu_toggle_log=0x7f05001a;
+        public static final int password=0x7f050017;
+        public static final int place=0x7f050011;
+        public static final int sample_content_fragment=0x7f050002;
+        public static final int sample_main_layout=0x7f050001;
+        public static final int sign_in_button=0x7f050018;
+        public static final int sliding_tabs=0x7f05000e;
+        public static final int textOutput2=0x7f050008;
+        public static final int time=0x7f050013;
+        public static final int title=0x7f050010;
+        public static final int viewpager=0x7f05000f;
     }
     public static final class layout {
         public static final int activity_display_post=0x7f030000;
@@ -63,32 +68,41 @@ public final class R {
         public static final int fragment_sample=0x7f030005;
         public static final int item=0x7f030006;
         public static final int item2=0x7f030007;
-        public static final int pager_item=0x7f030008;
+        public static final int login_activity=0x7f030008;
+        public static final int pager_item=0x7f030009;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int menu_display_post=0x7f070001;
+        public static final int main=0x7f080000;
+        public static final int menu_display_post=0x7f080001;
     }
     public static final class string {
-        public static final int Chat=0x7f050000;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050002;
-        public static final int buttonText=0x7f050003;
-        public static final int empty=0x7f050004;
-        public static final int hello_world=0x7f050005;
-        public static final int intro_message=0x7f050006;
-        public static final int messge=0x7f050007;
-        public static final int sample_hide_log=0x7f050008;
-        public static final int sample_show_log=0x7f050009;
-        public static final int text=0x7f05000a;
-        public static final int title_activity_display_post=0x7f05000b;
+        public static final int Chat=0x7f060000;
+        public static final int action_settings=0x7f060001;
+        public static final int action_sign_in=0x7f060002;
+        public static final int app_name=0x7f060003;
+        public static final int buttonText=0x7f060004;
+        public static final int empty=0x7f060005;
+        public static final int error_field_required=0x7f060006;
+        public static final int error_invalid_email=0x7f060007;
+        public static final int error_invalid_password=0x7f060008;
+        public static final int hello_world=0x7f060009;
+        public static final int intro_message=0x7f06000a;
+        public static final int logo=0x7f06000b;
+        public static final int main_activity_title=0x7f06000c;
+        public static final int messge=0x7f06000d;
+        public static final int prompt_email=0x7f06000e;
+        public static final int prompt_password=0x7f06000f;
+        public static final int sample_hide_log=0x7f060010;
+        public static final int sample_show_log=0x7f060011;
+        public static final int text=0x7f060012;
+        public static final int title_activity_display_post=0x7f060013;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
-        public static final int Theme_Base=0x7f060001;
-        public static final int Theme_Sample=0x7f060002;
-        public static final int Widget=0x7f060003;
-        public static final int Widget_SampleMessage=0x7f060004;
-        public static final int Widget_SampleMessageTile=0x7f060005;
+        public static final int AppTheme=0x7f070000;
+        public static final int Theme_Base=0x7f070001;
+        public static final int Theme_Sample=0x7f070002;
+        public static final int Widget=0x7f070003;
+        public static final int Widget_SampleMessage=0x7f070004;
+        public static final int Widget_SampleMessageTile=0x7f070005;
     }
 }
